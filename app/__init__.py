eec7f364d39c286df9df4abfccc0789d2be342c7
Amorti007 <dnzzrl19@gmail.com>
2025-06-21 15:21:45 +0300

    Update __init__.py and Create Flask App Factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,20 @@
-from flask import Flask
-from flask_pymongo import PyMongo
+from flask import Flask             # To create flask app
+from flask_pymongo import PyMongo   # To connect Flask and MongoDB
+from flask_cors import CORS         # To activate CORS for frontend
+import os                           # To read environment variables
+
+#Defining the central object for Flask and MongoDB connection
+mongo = PyMongo()
+
+#Defining the Flask App Factory
+def create_app():
+    app = Flask(__name__)
+
+    #Getting the connection address from environment variables
+    app.config["MONGO_URI"] = os.getenv("MONGO_URI","mongodb://localhost:27017/finance")
+
+    #Integration of MongoDB connection into application and frontend
+    mongo.init_app(app)
+    CORS(app)
+
+    return app
